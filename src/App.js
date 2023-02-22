5f85b6762fd52dbed753007a0d6e56daab0d2210
João Pedro Sena <sena.joaopedro42@gmail.com>
2023-02-22 11:18:45 +0100

    Revert "Working shopping list"
    This reverts commit 64fb707f23d1ebb81c0a0f4373b4e94e1b1e76a7.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react';
-import ShopList from './ShopList'
-import { v4 as uuidv4 } from 'uuid'
-
-const LOCAL_STORAGE_KEY = "shopApp.items"
 
 function App() {
-  const [items, setItems] = useState([]);
-  const itemNameRef = useRef();
-
-  useEffect(() => {
-    const storedItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    /* if (storedItems) setItems(prevItems => [...prevItems, ...storedItems]); */
-    if (storedItems) setItems(storedItems);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items))
-  }, [items]);
-
-  function toggleItem(id) {
-    const newItems = [...items];
-    const item = newItems.find(item => item.id === id);
-    
-    item.complete = !item.complete;
-    setItems(newItems);
-  }
-
-  function handleAddItem(e) {
-    const name = itemNameRef.current.value;
-
-    if (name === "") return;
-    setItems(prevItems => {
-      return [...prevItems, { id: uuidv4(), name: name, complete: false}];
-    })
-    itemNameRef.current.value = null;
-  }
-
-  function handleClearItems() {
-    const newItems = items.filter(item => !item.complete);
-    setItems(newItems);
-  }
-
-  return (
-    <>
-      <h1>LISTA DE COMPRAS</h1>
-      <ShopList items={items} toggleItem={toggleItem} />
-      <input ref={itemNameRef} type="text" />
-      <button onClick={handleAddItem}>Adicionar itens</button>
-      <button onClick={handleClearItems}>Limpar items selecioados</button>
-      <div>Ainda restam {items.filter(item => !item.complete).length} itens</div>
-    </>
-  );
+  return null;
 }
 
 export default App;
